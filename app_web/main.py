81481a841eeed5779f8d02ba074b32c2d7546945
RickyCode <rdelrioguzman@gmail.com>
2017-07-22 16:39:12 -0400

    mejoras urls

--- app_web/main.py
+++ app_web/main.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return '<h1> HOLA MUNDO</h1>'
+    return '<h1>CURSOS DE COMPUTACION</h1>'
 
 @app.route('/programacion_basico')
 def programacion_basico():
     return '<h1>Curso de Programación Básico</h1>'
 
 
+@app.route('/programacion_basico/git_webhook')
+def git_webhook():
+    return 'Se recibió la información'
